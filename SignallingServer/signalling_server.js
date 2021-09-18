f7e6f518143041629e6352d122d8fad2165b1789
b2jena <b2j1999jan27@gmail.com>
2021-09-18 13:12:08 +0530

    signalling server NodeJS implementation

--- SignallingServer/signalling_server.js
+++ SignallingServer/signalling_server.js
@@ -1,3 +1,17 @@
+// using websocket -> The primary interface for connecting to a WebSocket server and then sending and receiving data on the connection.
+// The WebSocket Protocol enables two-way communication between a client
+// running untrusted code in a controlled environment to a remote host
+// that has opted-in to communications from that code.  The security
+// model used for this is the origin-based security model commonly used
+// by web browsers.  The protocol consists of an opening handshake
+// followed by basic message framing, layered over TCP.  The goal of
+// this technology is to provide a mechanism for browser-based
+// applications that need two-way communication with servers that does
+// not rely on opening multiple HTTP connections (e.g., using
+// XMLHttpRequest or <iframe>s and long polling)
+
+// Resources that can help : https://spring.io/guides/gs/messaging-stomp-websocket/
+// https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8081 }, () => {
     console.log("Signalling server is now listening on port 8081");
